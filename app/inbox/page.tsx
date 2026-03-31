8b0c87994f624ef2fcbcc76ff3537dc869e190c6
Sayan <kosadm5@gmail.com>
2026-03-31 16:32:35 +0400

    feat: Add new test feature

--- app/inbox/page.tsx
+++ app/inbox/page.tsx
@@ -31,7 +31,7 @@ interface Message {
 // i18n Dictionary
 const dict = {
     ru: {
-        inbox: 'TSA Inbox',
+        inbox: 'TSA Inbox - test',
         support: 'Treema Support',
         activeChats: 'Активные чаты',
         noActive: 'Нет активных HITL чатов',
